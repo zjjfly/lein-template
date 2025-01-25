0652622f0e491627cb2f1ff62556d0ec2b3368c0
zjjfly <zjjblue@126.com>
2025-01-25 15:52:09 +0800

    update file structure

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject com.github.zjjfly/lein-template "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://www.github.com/zjjfly/lein-template"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [junit/junit "4.13.2"]]
   :main ^:skip-aot template.core
-  :plugins [[lein-cljfmt "0.8.2"]
+  :plugins [[lein-cljfmt "0.9.2"]
             [lein-junit "1.1.9"]]
   ;compile some namespaces ahead of time
   ;:aot [template.core]
@@ -15,11 +15,11 @@
   ;             "javac" "compile"]
   :target-path "target/%s"
   :global-vars {*warn-on-reflection* true}
-  :source-paths ["src/main/clj"]
-  :java-source-paths ["src/main/java" "src/test/java"]
-  :javac-options ["-target" "17" "-source" "17" "-encoding" "utf8" "-Xlint:-options"]
-  :resource-paths ["src/main/resources"]
-  :test-paths ["src/test/clj"]
-  :junit ["src/test/java"]
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java" "test/java"]
+  :javac-options ["-target" "21" "-source" "21" "-encoding" "utf8" "-Xlint:-options"]
+  :resource-paths ["resources"]
+  :test-paths ["test/clj"]
+  :junit ["test/java"]
   :profiles {:uberjar {:aot [template.core]}}
   )
